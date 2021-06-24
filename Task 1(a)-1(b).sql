4753fdac7a2d80f61cf2ed9f7c438cc9ee57db26
Ashpowerbi <86284159+Ashpowerbi@users.noreply.github.com>
2021-06-24 21:25:29 +1000

    Update Task 1(a)-1(b).sql

--- Task 1(a)-1(b).sql
+++ Task 1(a)-1(b).sql
@@ -5,6 +5,13 @@ from dbo.Property p join dbo.OwnerProperty t on p.Id = t.Id
 where t.OwnerId = 1426
 group by p.Name, t.OwnerId, t.PropertyId
 
+/*task1.a(redo)*/
+
+select Property.Name, OwnerProperty.PropertyId
+from dbo.Property inner join dbo.OwnerProperty on Property.Id = OwnerProperty.PropertyId
+where OwnerProperty.OwnerId = 1426
+
+
 /*task1.b*/
 
 SELECT Property.Name, Property.Id, PropertyHomeValue.Value,OwnerProperty.OwnerId AS Owner_ID
@@ -14,3 +21,10 @@ ON Property.Id = OwnerProperty.PropertyId
 INNER JOIN PropertyHomeValue
 ON Property.Id = PropertyHomeValue.PropertyId
 WHERE OwnerProperty.OwnerId = '1426'
+
+/*task1.b(redo)*/
+
+select Property.Name, OwnerProperty.PropertyId, PropertyFinance.CurrentHomeValue
+from dbo.Property INNER JOIN dbo.OwnerProperty on Property.Id = OwnerProperty.PropertyId
+INNER JOIN dbo.PropertyFinance on Property.Id = PropertyFinance.PropertyId
+where OwnerProperty.OwnerId = 1426
