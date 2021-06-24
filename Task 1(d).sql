6ce414b1ed7305f277a3cbc6e134743fb806c7a3
Ashpowerbi <86284159+Ashpowerbi@users.noreply.github.com>
2021-06-24 21:27:36 +1000

    Update Task 1(d).sql

--- Task 1(d).sql
+++ Task 1(d).sql
@@ -4,3 +4,17 @@ FROM Job
 INNER JOIN JobStatus
 ON Job.JobStatusId = JobStatus.Id
 WHERE JobStatus.Status = 'Open'
+
+/*task1.d*(redo)/
+
+select JobMedia.JobId,
+JobMedia.PropertyId,
+Job.OwnerId,
+Job.JobDescription,
+COUNT(JobStatusId) as NumberofJobMedia
+from JobMedia INNER JOIN Job on Job.Id = JobMedia.PropertyId
+where JobMedia.IsActive = 1
+GROUP BY JobMedia.JobId,
+JobMedia.PropertyId,
+Job.OwnerId,
+Job.JobDescription
